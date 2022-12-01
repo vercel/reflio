f7fa6333353abb7ed2bc05a8cf4e53390a746880
Elijah Cobb <elijah.cobb@vercel.com>
2022-12-01 10:58:55 -0500

    use secrets from env

--- apps/reflio/middleware.ts
+++ apps/reflio/middleware.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const VERCEL_BYPASS_TOKEN = process.env.VERCEL_BYPASS_TOKEN;
+const VERCEL_BYPASS_PASSWORD = process.env.VERCEL_BYPASS_PASSWORD;
+
+if (!VERCEL_BYPASS_TOKEN || !VERCEL_BYPASS_PASSWORD)
+  throw new Error('Vercel bypass token or password is not defined in env.');
+
 export const config = {
   matcher: ['/(.*)']
 };
@@ -12,12 +18,12 @@ export function middleware(req: NextRequest) {
       const [authType, authValue] = auth.split(' ');
       if (authType === 'Basic') {
         const password = atob(authValue).split(':')[1];
-        if (password === 'pass') {
+        if (password && password === VERCEL_BYPASS_PASSWORD) {
           return NextResponse.next();
         }
       }
       if (authType === 'Bearer') {
-        if (authValue === 'token') {
+        if (authValue && authValue === VERCEL_BYPASS_TOKEN) {
           return NextResponse.next();
         }
       }
