4f4b852f72864215149564ee5aadbf09c5eb4ccf
Elijah Cobb <elijah.cobb@vercel.com>
2022-12-01 10:47:17 -0500

    add back middleware auth

--- apps/reflio/middleware.ts
+++ apps/reflio/middleware.ts
@@ -5,27 +5,26 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
-  return NextResponse.next();
-  // const url = req.nextUrl;
-  // try {
-  //   const auth = req.headers.get('authorization');
-  //   if (auth) {
-  //     const [authType, authValue] = auth.split(' ');
-  //     if (authType === 'Basic') {
-  //       const password = atob(authValue).split(':')[1];
-  //       if (password === 'pass') {
-  //         return NextResponse.next();
-  //       }
-  //     }
-  //     if (authType === 'Bearer') {
-  //       if (authValue === 'token') {
-  //         return NextResponse.next();
-  //       }
-  //     }
-  //   }
-  // } catch (e) {
-  //   console.error(e);
-  // }
-  // url.pathname = '/api/auth';
-  // return NextResponse.rewrite(url);
+  const url = req.nextUrl;
+  try {
+    const auth = req.headers.get('authorization');
+    if (auth) {
+      const [authType, authValue] = auth.split(' ');
+      if (authType === 'Basic') {
+        const password = atob(authValue).split(':')[1];
+        if (password === 'pass') {
+          return NextResponse.next();
+        }
+      }
+      if (authType === 'Bearer') {
+        if (authValue === 'token') {
+          return NextResponse.next();
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  url.pathname = '/api/auth';
+  return NextResponse.rewrite(url);
 }
