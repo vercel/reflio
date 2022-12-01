ff216c822e3525d726ede22590c8f0f359871de2
Elijah Cobb <elijah.cobb@vercel.com>
2022-12-01 09:23:41 -0500

    removed auth dogfooding vercel auth

--- apps/reflio/middleware.ts
+++ apps/reflio/middleware.ts
@@ -5,26 +5,27 @@ export const config = {
 };
 
 export function middleware(req: NextRequest) {
-  const url = req.nextUrl;
-  try {
-    const auth = req.headers.get('authorization');
-    if (auth) {
-      const [authType, authValue] = auth.split(' ');
-      if (authType === 'Basic') {
-        const password = atob(authValue).split(':')[1];
-        if (password === 'pass') {
-          return NextResponse.next();
-        }
-      }
-      if (authType === 'Bearer') {
-        if (authValue === 'token') {
-          return NextResponse.next();
-        }
-      }
-    }
-  } catch (e) {
-    console.error(e);
-  }
-  url.pathname = '/api/auth';
-  return NextResponse.rewrite(url);
+  return NextResponse.next();
+  // const url = req.nextUrl;
+  // try {
+  //   const auth = req.headers.get('authorization');
+  //   if (auth) {
+  //     const [authType, authValue] = auth.split(' ');
+  //     if (authType === 'Basic') {
+  //       const password = atob(authValue).split(':')[1];
+  //       if (password === 'pass') {
+  //         return NextResponse.next();
+  //       }
+  //     }
+  //     if (authType === 'Bearer') {
+  //       if (authValue === 'token') {
+  //         return NextResponse.next();
+  //       }
+  //     }
+  //   }
+  // } catch (e) {
+  //   console.error(e);
+  // }
+  // url.pathname = '/api/auth';
+  // return NextResponse.rewrite(url);
 }
