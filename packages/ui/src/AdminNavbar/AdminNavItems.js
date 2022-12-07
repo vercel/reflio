9974fa8f7f8c6a34c20f70feac9b67a686833af0
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2022-12-07 15:16:19 +0000

    Remove billing artifacts

--- packages/ui/src/AdminNavbar/AdminNavItems.js
+++ packages/ui/src/AdminNavbar/AdminNavItems.js
@@ -186,20 +186,6 @@ export const AdminNavItems = () => {
         <div className="px-5 py-2">
           <p className="px-2 uppercase text-xs font-semibold text-gray-500 tracking-wide mb-2">Resources</p>
           {
-            planDetails === "free" &&
-            <Link
-              passHref
-              href="/pricing"
-              className={classNames(
-                navItemClass,
-                "text-secondary-2"
-              )} 
-            >
-              <GiftIcon className="mr-2 flex-shrink-0 h-5 w-5" aria-hidden="true" />
-              <span>Upgrade</span>
-            </Link>
-          }
-          {
             typeof Canny !== 'undefined' &&
             <div
               className={classNames(
