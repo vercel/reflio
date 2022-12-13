4c07c2dbf5d25d4e55b3ac2cb1595894c1b8e88d
Elijah Cobb <elijah.cobb@vercel.com>
2022-12-13 09:45:17 -0500

    add accepted check

--- apps/reflio/pages/api/affiliates/index.js
+++ apps/reflio/pages/api/affiliates/index.js
@@ -5,7 +5,9 @@ export default async function Endpoint(req, res) {
     .from('affiliates')
     .select('invite_email,name,vercel_username,affiliate_id')
     .not('name', 'is', null)
-    .not('vercel_username', 'is', null);
+    .not('vercel_username', 'is', null)
+    .eq('accepted', true)
+    .neq('vercel_username', '');
   if (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error.' });
